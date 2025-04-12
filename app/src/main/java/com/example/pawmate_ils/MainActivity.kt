39b1f6fe5601ea84a92cf6aa366c1abc346ec59e
Mika Lee <mikaellazabrinalee@gmail.com>
2025-04-12 19:13:44 +0800

    mika

--- app/src/main/java/com/example/pawmate_ils/MainActivity.kt
+++ app/src/main/java/com/example/pawmate_ils/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pawmate_ils
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,31 +18,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            PawMateILSTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+        setContentView(R.layout.main)
+
+
             }
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PawMateILSTheme {
-        Greeting("Android")
-    }
-}
