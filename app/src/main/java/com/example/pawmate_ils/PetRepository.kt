915ad85c7cdbdfff2f1f468fb757f0448dec0406
POWELL <164964498+saturra@users.noreply.github.com>
2025-04-17 03:29:38 +0800

    komet- PetRepos powell

---
+++ app/src/main/java/com/example/pawmate_ils/PetRepository.kt
@@ -0,0 +1,11 @@
+package com.example.pawmate_ils
+
+object PetRepository {
+    fun getPets(): List<Pet> {
+        return listOf(
+            Pet("Max", "1 year", "Playful and energetic", R.drawable.dog1),
+            Pet("Alex", "2 years", "Calm and loyal", R.drawable.dog2),
+            Pet("Bella", "6 months", "Curious and cuddly", R.drawable.dog3)
+        )
+    }
+}
