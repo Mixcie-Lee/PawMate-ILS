1285ccaad97d33aced5b39262864fbfbda635a97
POWELL <164964498+saturra@users.noreply.github.com>
2025-04-17 18:17:57 +0800

    Changes new

---
+++ app/src/main/java/com/example/pawmate_ils/Pet.kt
@@ -0,0 +1,8 @@
+package com.example.pawmate_ils
+
+data class Pet(
+    val name: String,
+    val age: String,
+    val description: String,
+    val imageRes: Int // Resource ID for the pet image
+)
