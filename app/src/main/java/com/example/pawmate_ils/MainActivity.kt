5a731879d1b53a6283b6538435c007a2bf6c38a6
ehrnestga <ehrnest.ga2006@gmail.com>
2025-04-18 23:38:30 +0800

    KT stuff and home screen stuff

--- app/src/main/java/com/example/pawmate_ils/MainActivity.kt
+++ app/src/main/java/com/example/pawmate_ils/MainActivity.kt
@@ -7,8 +7,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pawmate_ils.ui.screens.LoginScreen
+import com.example.pawmate_ils.ui.screens.SellerSignUpScreen
+import com.example.pawmate_ils.ui.screens.SignUpScreen
+import com.example.pawmate_ils.ui.screens.UserTypeSelectionScreen
 import com.example.pawmate_ils.ui.theme.PawMateILSTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,14 +25,67 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            PawMateILSTheme{
-                Scaffold { innerPadding ->
-                    Box(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    ) {
-                        PetSwipeScreen(userName = "Alex")
+            PawMateILSTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Scaffold { innerPadding ->
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding)
+                        ) {
+                            val navController = rememberNavController()
+                            
+                            NavHost(navController = navController, startDestination = "user_type") {
+                                composable("user_type") {
+                                    UserTypeSelectionScreen(navController = navController)
+                                }
+                                composable("signup") {
+                                    SignUpScreen(
+                                        onSignUpClick = { _, _, _, _ ->
+                                            navController.navigate("swipe")
+                                        },
+                                        onLoginClick = { 
+                                            navController.navigate("login")
+                                        },
+                                        onSellerAuthClick = {
+                                            navController.navigate("seller_signup")
+                                        }
+                                    )
+                                }
+                                composable("login") {
+                                    LoginScreen(
+                                        onLoginClick = { _, _ ->
+                                            navController.navigate("swipe")
+                                        },
+                                        onSignUpClick = {
+                                            navController.navigate("signup")
+                                        },
+                                        onSellerAuthClick = {
+                                            navController.navigate("seller_signup")
+                                        }
+                                    )
+                                }
+                                composable("seller_signup") {
+                                    SellerSignUpScreen(
+                                        onSignUpClick = { _, _, _, _, _, _ ->
+                                            navController.navigate("swipe")
+                                        },
+                                        onLoginClick = {
+                                            navController.navigate("login")
+                                        },
+                                        onUserAuthClick = {
+                                            navController.navigate("signup")
+                                        }
+                                    )
+                                }
+                                composable("swipe") {
+                                    PetSwipeScreen(userName = "Alex")
+                                }
+                            }
+                        }
                     }
                 }
             }
--- app/src/main/java/com/example/pawmate_ils/Pet.kt
+++ app/src/main/java/com/example/pawmate_ils/Pet.kt
@@ -4,5 +4,5 @@ data class Pet(
     val name: String,
     val age: String,
     val description: String,
-    val imageRes: Int // Resource ID for the pet image
+    val imageRes: Int 
 )
